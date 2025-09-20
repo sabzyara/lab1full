0b301eaec2565b661b93f7672e0eccae826aa4be
sabzyara <s.dzhumagazieva15@gmail.com>
2025-09-20 14:51:11 +0500

    first commit

---
+++ src/main/java/com/example/fullstack1/Fullstack1Application.java
@@ -0,0 +1,13 @@
+package com.example.fullstack1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fullstack1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fullstack1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fullstack1/Fullstack1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstack1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fullstack1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
